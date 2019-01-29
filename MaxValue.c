b27f8f0678625765115d0c1dd1561187307bd94c
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-01-29 17:12:58 +0700

    Update MaxValue.c

--- MaxValue.c
+++ MaxValue.c
@@ -3,25 +3,28 @@
 *Source file name:
 *Author: Tamkien Cao
 *Last modified: Thu, Jan 24th, 2019
+*Version: 1.0
+*Changelog:
+-Translate everything into English.
 *Other info:
 -This is the 9th exercise from the workshop.
-**/
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 main()
 {
 	int n, maxx, i, a;
-	printf("Phan mem nay se giup ban tim ra so lon nhat trong day so cua ban.\n");
-	printf("Day so cua ban co bao nhieu so?\n");
+	printf("This program can help you find out the largest number in your list.\n");
+	printf("How many numbers are there?\n");
 	scanf_s("%d", &n);//phai lay so nay de xac dinh so vong lap
-	printf("Nhap so thu nhat:\n");
+	printf("Insert the first number here:\n");
 	scanf_s("%d", &maxx);//neu co so lon hon thi se thay the gia tri do vao max
 	for (i = 2; i <= n; i++)
 		//vong lap, bat dau tu so thu hai
 		//day la lap de lay so lieu
 	{
-		printf("Nhap so thu %d:\n", i);
+		printf("Insert the next number here:\n", i);
 		scanf_s("%d", &a);
 		if (a > maxx)
 		{
@@ -29,7 +32,7 @@ main()
 		}
 
 	}
-	printf("So lon nhat trong day la %d", maxx);
+	printf("The largest number in your list is %d", maxx);
 	printf("\n=================================\n");
 	printf("Written by Tamkien Cao. Thank you for using my application!\n");
 	//credit line, neu xoa dong nay ctrinh se ko chay duoc;
