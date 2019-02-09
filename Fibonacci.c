fe8be1475e4f2d0a885bd4fadca68ad5266bfbe7
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-02-09 21:24:33 +0700

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -2,9 +2,13 @@
 *File name:
 *Source file name:
 *Author: Tamkien Cao
-*Last modified: 13:27 Fri, Jan 18th, 2019
+*Last modified: Sat, Feb 9th, 2019
+*Version: 1.0 Final
+*Changelog: 
+- Translate everything into English.
+- Add some more reasonable comments.
 *Other info:
--This ís the 17th exercise from the workshop.
+-This Ã­s the 17th exercise from the workshop.
 **/
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,36 +16,42 @@
 main()
 {
 	int i, n, F = 0, F1 = 1, F2;
-	printf("Phan mem nay se giup ban viet ra mot day Fibonacci voi do dai tuy thich.\n");
-	printf("Day Fibonacci co dang Fn = F(n-1) + F(n-2) va bat dau tu hai so hang 0, 1.\n");
-	printf("Ban muon mot day co bao nhieu so?\n");
+	/*
+	*i is the ordinal number in the sequence
+	*n is the number of numbers :D in the sequence
+	*F and F1 is the first and second number in the addition
+	*F2 is always the number I need to print.
+	*/
+	printf("This application can create a Fibonacci sequence with a given length.\n");
+	printf("A Fibonacci sequence is created by the following formula: Fn = F(n-1) + F(n-2)/nAnd started with 0 and 1.\n");
+	printf("How many numbers do you want in the sequence?\n");
 	scanf_s("%d", &n);
 	if (n <= 2)
 	{
-		printf("Day so Fibonacci co it nhat 3 so hang!");
+		printf("A Fibonacci sequence has at least 3 numbers.!");
 	}
 	else
 	{
-		printf("Day Fibonacci cua ban la 0, 1, ");
+		printf("Your Fibonacci sequence is 0, 1, ");
 		for (i = 3; i <= n; i++)
-			//bat dau tu so 3 vi da co 2 so dau la 0 va 1.
+			//starting with 3 as the first ones are 0 and 1.
 		{
-			F2 = F + F1;//Day co dang F, F1, F2, F3, F4, vv
-			if (i < n)//khi do con dau phay
+			F2 = F + F1;//The sequence is like F, F1, F2, F3, F4, etc.
+			if (i < n)//My purpose is that the numbers are separated by a comma,
 			{
-				printf("%d, ", F2);//lay F2 truoc
+				printf("%d, ", F2);
 			}
-			else if (i = n)//truong hop so cuoi cung nen co dau cham.
+			else if (i = n)// and the last one end with a dot.
 			{
 				printf("%d.", F2);
 			}
-			F = F1;//o F3 thi F thanh F1
-			F1 = F2;//con F1 thanh F2. Khong duoc lam nguoc lai. Hay nho bai trao so trong assembly.
+			F = F1;//as we calculate F3, F3 is F2 and F1 is F in the 37th line
+			F1 = F2;//and F2 is F1. 
 		}
 	}
 	printf("\n=================================\n");
 	printf("Written by Tamkien Cao. Thank you for using my application!\n");
-	//credit line, neu xoa dong nay ctrinh se ko chay duoc;
+	//credit line. Please behave ethically by not fucking deleting it.
 	system("pause");
 }
 
