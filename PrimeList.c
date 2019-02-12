1675f8d66080d3bc73a5fb327cf93077e5d7f9f4
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-02-12 14:04:35 +0700

    Update PrimeList.c

--- PrimeList.c
+++ PrimeList.c
@@ -3,8 +3,9 @@
 *Source file name:
 *Author: Tamkien Cao
 *Last modified: Tue, Feb 12th, 2019
-*Version: 1.0
-*Changelog(s): No.
+*Version: 2.0
+*Changelog(s): 
+- Fix a critical bug that fucks the application if n==2.
 *Other info:
 -This is an exercise.
 **/
@@ -14,26 +15,34 @@ main()
 {
 	int n, i, k;
 	printf("This program can print prime numbers that is not greater than your number.\n");
-	printf("Enter your number here:\n");
+	printf("Enter an integer that is larger than 1 here:\n");
 	scanf_s("%d", &n);
-	printf("The prime numbers are 2, 3");
-	for ( i = 2; i <= n; i++)
-		//i will run from 2 to n
+	if (n==2)
 	{
-		for (k = 2; k <= i/2; k++)
-			//k will run from 2 to i/2
+		printf("The only prime number is 2.");
+	}
+	else if (n > 2)
+	{
+
+		printf("The prime numbers are 2, 3");//my calculating can't print 2 and 3 so this is my solution lol
+		for (i = 2; i <= n; i++)
+			//i will run from 2 to n
 		{
-			if (i%k == 0)// which means i is not a prime number. 
+			for (k = 2; k <= i / 2; k++)
+				//k will run from 2 to i/2
 			{
-				break;//the "for" of the 23rd line will stop, the one of the 20th line will continue to run
-			}
-			else if (k==i/2)//which mean i is a prime number
-			{
-				printf(", %d", i);// print i, then the 20th line will continue to run
+				if (i%k == 0)// which means i is not a prime number. 
+				{
+					break;//the "for" of the 31st line will stop, the one of the 28th line will continue to run
+				}
+				else if (k == i / 2)//which means i is a prime number
+				{
+					printf(", %d", i);// print i, then the 28th line will continue to run
+				}
 			}
 		}
+		printf(".");//a tiny cute dot at the end
 	}
-	printf(".");//a tiny cute dot at the end
 
 	printf("\n=================================\n");
 	printf("Written by Tamkien Cao. Thank you for using my application!\n");
