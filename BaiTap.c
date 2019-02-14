dba1db2e54f30721479dd019350683e773b72aa3
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-02-14 13:05:23 +0700

    Update BaiTap.c

--- BaiTap.c
+++ BaiTap.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 main()
 {
 	char name[20];
 	int ngaycong, luongngay, luongthang, tamung;
 	printf("Nhap ho ten:\n");
-	scanf("%[^\n]s", &name);
+	gets(name);
 	printf("Nhap so ngay cong:\n");
 	scanf("%d", &ngaycong);
 	printf("Nhap so tien luong mot ngay:\n");
@@ -13,10 +14,11 @@ main()
 	printf("Nhap so tien tam ung cuoi thang:\n");
 	scanf("%d", &tamung);
 	luongthang = luongngay * ngaycong - tamung;
-	printf("Ho va ten: %s\n", name);
+	printf("Ho va ten: ");
+	puts(name);
 	printf("So ngay cong: %d\n", ngaycong);
 	printf("Luong mot ngay: %d\n", luongngay);
 	printf("Tam ung tien cuoi thang:%d\n", tamung);
-	printf("Luong thang: %d", luongthang);
+	printf("Luong thang: %d\n", luongthang);
 	system("pause");
 }
