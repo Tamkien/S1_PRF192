f78ff73d21cc68b51e8db1fbea2f389e7c63d9c7
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-02-26 13:19:23 +0700

    Update LCM_Euclid.c

--- LCM_Euclid.c
+++ LCM_Euclid.c
@@ -2,49 +2,64 @@
 *File name:
 *Source file name:
 *Author: Tamkien Cao
-*Last modified: Thu, Jan 24th, 2019
-*Version: 1.0
-*Changelog: 
--Translate everything into English.
--Edited some disordering brackets.
--Added some comments.
+*Last modified: Tue, Feb 26th, 2019
+*Version: 2.0
+*Changelog:
+- Used function.
 *Other info:
 -This is the 20th exercise from the workshop.
 -[README] The idea had been hacked from http://bit.do/eGvJu
 **/
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-main()
+int LCM(int a, int b)
 {
-	int a,b,x;
-	printf("This program can find out the largest common multiplier (LCM) of two numbers.\n");
-	printf("Insert the larger number here:\n");
-	scanf_s("%d", &a);
-	printf("Insert the smaller number here:\n");
-	scanf_s("%d", &b);
-	while (1<2)//always true
+	int x;
+	while (1)//always true
 	{
-	x = a % b;
-	if (x == 0)
+		x = a % b;
+		if (x == 0)
 		{
-			printf("The LCM of your numbers is %d.", b);
+
 			break;//see the reason in the link. 
 		}
-	else
+		else
 		{
 			a = b;
 			b = x;
 		}
 	}
-	
+	return b;
+}
+main()
+{
+	int a, b;
+	printf("This program can find out the largest common multiplier (LCM) of two numbers.\n");
+	do
+	{
+		printf("Insert the greater number here, enter 0 to exit:\n");
+		scanf_s("%d", &a);
+		if (a == 0)
+		{
+			break;
+		}
+		else
+		{
+			printf("Insert the smaller number here:\n");
+			scanf_s("%d", &b);
+			if (b > a)
+			{
+				printf("Hey, enter the greater number first.\n");
+			}
+			else
+			{
+				printf("The LCM of your numbers is %d.\n", LCM(a, b));
 
-	
-
-
+			}
+		}
+	} while (1);
 	printf("\n=================================\n");
 	printf("Written by Tamkien Cao. Thank you for using my application!\n");
 	//credit line, neu xoa dong nay ctrinh se ko chay duoc;
 	system("pause");
 }
-
