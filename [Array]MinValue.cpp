1b9d927b4e8920aa20342e64aa17a46baeaf48b6
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2019-03-07 13:35:57 +0700

    Create [Array]MinValue.cpp

---
+++ [Array]MinValue.cpp
@@ -0,0 +1,37 @@
+/*
+*File name:
+*Source file name:
+*Author: Tamkien Cao
+*Last modified: Thu, Mar 7th, 2019
+*Other info:
+-Exercise 5.3
+*/
+#include <stdio.h>
+#include <stdlib.h>
+void min()
+{
+	int a[100], thutu;
+	printf("Phan mem nay giup ban tim ra so nho nhat trong day cua ban.\n");
+	for (int i = 1; i < INT_MAX; i++)
+	{
+		printf("Nhap n = ");
+		scanf_s("%d", &a[i]);
+		if (a[i]==0)
+		{
+			break;
+		}
+		else if (a[i]<a[1])
+		{
+			a[1] = a[i];
+			thutu = i;
+		}
+	}
+	printf("So nho nhat la %d, va do la so thu %d ma ban nhap vao.\n", a[1], thutu);
+}
+int main()
+{
+	min();
+	printf("\n=================================\n");
+	printf("Written by Tamkien Cao. Thank you for using my application!\n");
+	system("pause");
+}
